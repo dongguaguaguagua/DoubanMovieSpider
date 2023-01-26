c94bab2e39b4d12992413e397f2fc1968a90dd79
dongguaguaguagua <1448424184@qq.com>
2023-01-26 19:04:28 +0800

    test

--- test.py
+++ test.py
@@ -1,34 +1,58 @@
 # _*_ coding:utf-8 _*_
-from  urllib import request
-from fake_useragent import UserAgent
-from pandas.core import resample
+# from bs4 import BeautifulSoup
+# from update import *
+
+# soup=BeautifulSoup("","lxml")
+
+
+# with open("example.html","r") as file:
+# 	resp=file.read()
+
+# soup = BeautifulSoup(resp,'lxml')
+
+# print(update_data(soup))
+
+# -*- coding: utf-8 -*-
 import requests
-import datetime
+import json
+import random
 from bs4 import BeautifulSoup
+import sys
+import time
+from fake_useragent import UserAgent
 from update import *
 
-soup=BeautifulSoup("","lxml")
+if __name__ == "__main__":
+    i=0
+    with open("dic1.json","r") as file:
+        data=json.load(file)
+
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+    while(i<2):
+        if(i%20==19):
+            headers = {'User-Agent': ua.random}
+            print("更新ua成功")
+
+        resp = requests.get("http://www.douban.com/subject/35766491", headers = headers)
+
+        if(resp.status_code != 200):
+            if(resp.status_code == 404):
+                print("subject=",data[str(i)],"的页面找不到，执行下一个")
+                i+=1
+                continue
+            print("错误码 : ",resp.status_code)
+            continue
+
+        soup = BeautifulSoup(resp.text, 'lxml')
+        if(soup == ""):
+            print("soup为空，尝试更新proxy")
+            continue
+        print("生成第",i,"个电影数据……",resp.status_code)
+        dataLine = update_data(soup)
+        # 生成数据
+        os.system("echo "+str(i)+","+str(data[str(i)])+","+dataLine+" >> MovieData.csv")
+        time.sleep(random.random()*3)
+        i+=1
 
 
-with open("example.html","r") as file:
-	resp=file.read()
-
-soup = BeautifulSoup(resp,'lxml')
-
-update_data(soup)
-
-
-print("上映年份:",year)
-print("电影名称:",name)
-print("海报链接:",post_link)
-print("豆瓣评分:",rating)
-print("评分个数:",rating_people)
-print("短评数:",short_rating_num)
-print("影评数:",review_num)
-print("电影时长:",movie_length)
-print("上映日期:",release_date)
-print("导演:",director)
-print("编剧:",playwright)
-print("演员:",actors)
-print("体裁:",genre)
-print("国家:",country)
